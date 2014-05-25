29eb02eb57fd03c634e207d6e93bb377eb4b1ade
IrisTien <tq05s1l@126.com>
2014-05-25 11:27:43 +0800

    the main page

---
+++ Main.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="main" runat="server">
+    <title>实时信息监控系统</title>
+    <link href="Scripts/extjs-4.2.1/resources/css/ext-all-neptune.css" rel="stylesheet" type="text/css" />
+    <script src="Scripts/extjs-4.2.1/ext-all.js" type="text/javascript"></script>
+    <script src="Scripts/extjs-4.2.1/locale/ext-lang-zh_CN.js" type="text/javascript"></script>
+    <script src="Scripts/main.js" type="text/javascript"></script>
+    
+</head>
+<body>
+</body>
+</html>
---
+++ Scripts/main.js
@@ -0,0 +1,76 @@
+﻿Ext.application({
+    name: "HelloExt",
+    launch: function () {
+        Ext.create('Ext.panel.Panel', {
+            width: 1024,
+            height: 720,
+            layout: 'border',
+            items: [{
+                region: 'north',
+                xtype: 'panel',
+                height: 100,
+                html: '<h>系统信息实时监控系统</h>',
+                margins: '0 5 5 5'
+            }, {
+                region: 'south',
+                xtype: 'panel',
+                height: 20,
+                split: false,
+                html: '欢迎登录!',
+                margins: '0 5 5 5'
+            }, {
+                title: '菜单栏',
+                region: 'west',
+                layout:'anchor',
+                xtype: 'panel',
+                margins: '5 0 0 5',
+                width: 200,
+                collapsible: true,
+                id: 'west-region-container',
+                layout: 'fit',
+                items: [
+                    {
+                        xtype: 'button',
+                        text: 'click me',
+                        height: 50,
+                        scale: 'medium',
+                        handler: function() {
+                            alert("you clicked the button");
+                        }
+                    }, {
+                        xtype: 'button',
+                        text: 'click me2',
+                        scale: 'medium',
+                        handler: function () {
+                            alert("you clicked the button");
+                        }
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'click me3',
+                        scale: 'medium',
+                        handler: function () {
+                            alert("you clicked the button");
+                        }
+                    }, {
+                        xtype: 'button',
+                        text: 'click me4',
+                        scale: 'medium',
+                        handler: function () {
+                            alert("you clicked the button");
+                        }
+                    }
+
+                ]
+            }, {
+                title: '内容',
+                region: 'center',
+                xtype: 'panel',
+                layout: 'fit',
+                margins: '5 5 0 0',
+                html: '这是中间'
+            }],
+            renderTo: Ext.getBody()
+        });
+    }
+});
